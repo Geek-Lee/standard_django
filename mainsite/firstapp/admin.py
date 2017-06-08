1192275a90a1b92337d645805d6e19755b4eac8a
Geek-Lee <2546701377@qq.com>
2017-06-08 21:15:20 +0800

    文章管理

--- mainsite/firstapp/admin.py
+++ mainsite/firstapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from firstapp.models import People, Article
 
 # Register your models here.
+admin.site.register(People)
+admin.site.register(Article)
--- mainsite/firstapp/views.py
+++ mainsite/firstapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from firstapp.models import People, Article
+from django.template import Context, Template
 
 # Create your views here.
+def index(request):
+    context = {}
+    article_list = Article.objects.all()
+    context['article_list'] = article_list
+    index_page = render(request, 'first_web_2.html', context)
+    return index_page
